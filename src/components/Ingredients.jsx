d8947d5dfbb733337260df07aeef9e0a0351b3c1
nadomola <siyun778@gmail.com>
2025-01-26 21:02:04 +0900

    :lipstick: 재료 요소 변경
    재료 요소 변경

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,22 +1,24 @@
 
 const Ingredients = [ 
 
-    {id:1, name: "msg", img: "/msg.png"},
-    {id:2, name: "가다랑어포", img: "/가다랑어포.png"},
-    {id:3, name: "가리비", img: "/가리비.png"},
-    {id:4, name: "가지", img: "/가지.png"},
-    {id:5, name: "간장", img: "/간장.png"},
-    {id:6, name: "간장게장", img: "/간장게장.png"},
-    {id:7, name: "감자", img: "/감자.png"},
-    {id:8, name: "갓김치", img: "/갓김치.png"},
-    {id:9, name: "강낭콩", img: "/강낭콩.png"},
-    {id:10, name: "강황가루", img: "/강황가루.png"},
-    {id:11, name: "건과일", img: "/건과일.png"},
-    {id:12, name: "건어물", img: "/건어물.png"},
-    {id:13, name: "게", img: "/게.png"},
-    {id:14, name: "겨자", img: "/겨자.png"},
-    {id:15, name: "고등어", img: "/고등어.png"},
-    {id:16, name: "가자미", img: "/가자미.png"}
+    {id:1, name: "감자", img: "/감자.png"},
+    {id:2, name: "고추", img: "/고추.png"},
+    {id:3, name: "계란", img: "/계란.png"},
+    {id:4, name: "김치", img: "/김치.png"},
+    {id:5, name: "당근", img: "/당근.png"},
+    {id:6, name: "돼지고기", img: "/돼지고기.png"},
+    {id:7, name: "두부", img: "/두부.png"},
+    {id:8, name: "마늘", img: "/마늘.png"},
+    {id:9, name: "버섯", img: "/버섯.png"},
+    {id:10, name: "빵", img: "/빵.png"},
+    {id:11, name: "새우", img: "/새우.png"},
+    {id:12, name: "생선", img: "/생선.png"},
+    {id:13, name: "애호박", img: "/애호박.png"},
+    {id:14, name: "양배추", img: "/양배추.png"},
+    {id:15, name: "양파", img: "/양파.png"},
+    {id:16, name: "오이", img: "/오이.png"},
+    {id:17, name: "파스타면", img: "/파스타면.png"},
+    {id:18, name: "햄", img: "/햄.png"},
 ];
 
 export default Ingredients;
