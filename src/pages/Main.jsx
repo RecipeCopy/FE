7b78ecdc61b4bb8db3848487493e26c08b0c7d86
heeone1 <rinakim1019@gmail.com>
2025-01-27 18:48:18 +0900

    [Design] Tabbar 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import TabBar from "../components/TabBar";
-import styled from "styled-components";
 
 const Main = () => {
     return (
         <>
-            <MainContainer>
-                <Header />
-                <TabBar />
-            </MainContainer>
+          <Header />
+          <TabBar />
         </>
     );
   };
   
 export default Main;
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  min-height: 100vh;
-`;
-
