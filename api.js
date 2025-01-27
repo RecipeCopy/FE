2e32cf5cbbc465a5744f4885800f03af379baa41
nadomola <siyun778@gmail.com>
2025-01-27 13:56:05 +0900

    :poop: Api.js 수정
    api.js 수정

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/swagger-ui/index.html";
 
 export async function fetchRecipes() {
     try {
       const response = await axios.get(`${API_BASE_URL}/api/recipes`);
-      return response.data();
+      return response.data;
     } catch (error) {
       console.error("Error fetching recipes:", error);
       throw error;
